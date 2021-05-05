043970ec5899d7f4e15369c4604107fadb3f9760
cypher33 <rohitkumar25.rkl@gmail.com>
2021-05-05 23:35:03 +0530

    learnt about redux reducers

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -5,8 +5,10 @@ import { connect } from 'react-redux';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assests/crown.svg';
 import { auth } from './../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link to="/">
@@ -27,13 +29,16 @@ const Header = ({ currentUser }) => {
                         SIGN IN
                     </Link>
                 }
+                <CartIcon />
             </div>
+            {hidden ? null : <CartDropdown />}
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
